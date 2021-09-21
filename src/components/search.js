998e59392b0f20b7878c4f00416b450a9c4978a6
udbhav1 <umuthakana@gmail.com>
2021-09-21 19:31:06 -0400

    Style search results

--- src/components/search.js
+++ src/components/search.js
@@ -13,8 +13,9 @@ const Search = (props) => {
     const location = useLocation();
     const [songs, setSongs] = useState([]);
     const [songLyrics, setSongLyrics] = useState([]);
-    const [songLoading, setSongLoading] = useState(false);
+    // const [songLoading, setSongLoading] = useState(false);
     const [searchQuery, setSearchQuery] = useState("");
+    const [totalSongs, setTotalSongs] = useState(0);
     const filteredSongs = filterSongs(songLyrics, searchQuery);
 
     const spotifyApiLink = "https://api.spotify.com/v1/playlists/";
@@ -23,8 +24,12 @@ const Search = (props) => {
         return {name: songName, artist: songArtist, id: songId};
     }
 
-    function makeLyricsObject(songName, songLyrics, songId){
-        return {name: songName, lyrics: songLyrics, id: songId};
+    function makeLyricsObject(songName, songArtist, songLyrics, songId){
+        let sLyrics = [];
+        if(songLyrics != null){
+          sLyrics = songLyrics.split("\n");
+        }
+        return {name: songName, artist: songArtist, lyrics: sLyrics, id: songId};
     }
 
     function cleanSongName(oldName){
@@ -43,7 +48,7 @@ const Search = (props) => {
         }
         return entries.filter((entry) => {
             const curLyrics = entry.lyrics;
-            return curLyrics != null && curLyrics.toLowerCase().includes(query.toLowerCase());
+            return curLyrics != null && curLyrics.join(" ").toLowerCase().includes(query.toLowerCase());
 
         });
     }
@@ -65,7 +70,6 @@ const Search = (props) => {
     async function getTracks(playlistId){
         // if we've already loaded the songs 
         if(songs.length > 0){
-          setSongLoading(false);
           return;
         }
         let initialFetch = await getNext100(spotifyApiLink + playlistId);
@@ -76,6 +80,7 @@ const Search = (props) => {
         setSongs([]);
 
         console.log("Total songs: " + totSongs);
+        setTotalSongs(totSongs);
 
         for(let track of curData.items){
           // console.log(makeSongObject(track.track.name, track.track.artists[0].name, track.track.name));
@@ -87,7 +92,7 @@ const Search = (props) => {
         while(songsProcessed < totSongs){
           let nextLink = curData.next;
           curData = await getNext100(nextLink);
-          console.log(curData);
+          // console.log(curData);
 
           for(let track of curData.items){
             setSongs(oldArray => [...oldArray, makeSongObject(track.track.name, track.track.artists[0].name, track.track.id)]);
@@ -95,7 +100,7 @@ const Search = (props) => {
             songsProcessed += 1;
           }
         }
-        setSongLoading(false);
+        // setSongLoading(false);
     }
 
     async function getTrackLyrics(trackName, trackArtist, trackId){
@@ -105,13 +110,13 @@ const Search = (props) => {
           artist: trackArtist,
           optimizeQuery: true,
         };
-        console.log(geniusOptions);
+        // console.log(geniusOptions);
 
-        getLyrics(geniusOptions).then((lyrics) => setSongLyrics(oldArray => [...oldArray, makeLyricsObject(trackName, lyrics, trackId)]));
+        getLyrics(geniusOptions).then((lyrics) => setSongLyrics(oldArray => [...oldArray, makeLyricsObject(trackName, trackArtist, lyrics, trackId)]));
     }
 
     useEffect(() => {
-        setSongLoading(true);
+        // setSongLoading(true);
         getTracks(location.state.playlistId);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }  , []);
@@ -120,34 +125,32 @@ const Search = (props) => {
       <div>
           {location.state ? (
             <div>
-              <p>Songs length: {songs.length}</p>
-              <p>Lyrics length: {songLyrics.length}</p>
 
               <div id="searchBarContainer">
                 <SearchBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
               </div>
 
-              <p>Songs filtered by "{searchQuery}":</p>
+              {songLyrics.length < totalSongs &&
+                <div id="infoContainer">
+                  {/* <div className="loader"></div> */}
+                  <p id="progressInfo">Songs Processed: {songs.length}/{totalSongs}</p>
+                  <p id="progressInfo">Lyrics Processed: {songLyrics.length}/{totalSongs}</p>
+                </div>
+              }
 
-              <div>
                 {filteredSongs.map(entry =>
-                  <div id="searchResult">
-                    <details key={entry.id}>
-                        <summary>{entry.name}</summary>
+                  <div id="searchResult" key={entry.id}>
+                    <details>
+                        <summary id="songTitle"><b>{entry.name}</b> - {entry.artist}</summary>
 
-                        <p>{entry.lyrics}</p>
+                        <hr/> 
+
+                        {entry.lyrics.map((line, index) =>
+                            <p key={index}>{line}</p>
+                        )}
                     </details>
                   </div>
                 )}
-              </div>
-
-              <div className="loader"></div>
-
-              {songLoading ? (
-                <div className="loader"></div>
-              ) : (
-                <p>done</p>
-              )}
 
             </div>
           ) : (
